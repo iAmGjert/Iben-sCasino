6301e05cc7bf346bc428693cedb6e08970ec521c
Joetags <tangney6@gmail.com>
2021-09-23 17:02:44 -0500

    initial set up complete

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+
+  mode: "development",
+  entry: path.resolve(__dirname, "client", "src", "index.jsx"),
+  output: {
+    filename: "bundles.js",
+    path: path.resolve(__dirname, "client", "dist")
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  }
+
+}
