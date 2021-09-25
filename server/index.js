31cb9cf39e921c2f77ca75e1bd98b128494b110e
Alex McGinnis <alex.z.mcginnis@gmail.com>
2021-09-25 16:30:45 -0500

    trying to resolve merge conflicts in server index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const blj = require('./routes/blackjack')
+//const blj = require('./routes/blackjack')
+
+  
+const passport = require('passport');
+const authenticate = require('./authenticate.js');
+const { Data } = require('./routes/userDatabase');
+
 
 const port = 1337;
 
@@ -10,8 +16,20 @@ app.use(express.json())
 const frontend = path.resolve(__dirname, '..', 'client', 'dist');
 
 app.use('/routes/blackjack', blj.Blackjack)
+app.use('/routes/userDatabase', Data);
 
 app.use(express.static(frontend));
+
+app.use(passport.initialize());
+app.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+
+app.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    //res.redirect('/'); --> to the main game page
+    res.send('Logged In!');
+  });
 
 
 app.listen(port, () => {
