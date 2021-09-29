df2d5a0d6d7c2ec9e92677ca281c7ab244c1435e
Joetags <tangney6@gmail.com>
2021-09-29 09:39:08 -0500

    console.logs go bye bye

--- server/routes/clientId.js
+++ server/routes/clientId.js
@@ -6,8 +6,7 @@ const cId = require('dotenv').config();
 ClientId.get('/', async (req, res) => {
   
   try {
-    console.log('endpt');
-    console.log('cid', cId);
+    
     res.status(201).send(cId);
   } catch (err) {
     console.log(err);
