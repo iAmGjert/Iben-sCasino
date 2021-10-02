1827a4ede982902ecf008a2488dd10f191e02bdf
Joetags <tangney6@gmail.com>
2021-10-01 20:41:06 -0500

    navbar profile page styles

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -7,17 +7,26 @@ const NavBar = (props) => {
   
 
   return (
-    <div>
-      <p>
-        <Link to='/blackjack'>Blackjack</Link>
-        <Link to='/login'>Login</Link>
-        <Link to='/profile'>Profile</Link>
-        <Link to='/addFriends'>Interacting with Users</Link>
-        <Link to='/logout'>Logout</Link>
-      </p>
+    
+    <nav className="red darken-4">
+      <div className="nav-wrapper container">
+        <div className="row right hide-on-med-and-down">
+          <div className="col s2.5"><Link to='/blackjack'>Blackjack</Link></div>
+          <div className="col s2.5"><Link to='/login'>Login</Link></div>
+          <div className="col s2.5"><Link to='/profile'>Profile</Link></div>
+          <div className="col s2.5"><Link to='/addFriends'>Interacting with Users</Link></div>
+          <div className="col s2.5"><Link to='/logout'>Logout</Link></div>
+        </div>
+          
+          
+          
+          
+          
+      </div>
+      
+    </nav>
         
-        
-    </div>
+    
   );
   
 };
