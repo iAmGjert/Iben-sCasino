5eb6d2777835a3f254780d140820d7e04751f17f
Alex McGinnis <alex.z.mcginnis@gmail.com>
2021-10-03 17:05:58 -0500

    typo in moneyOnTable fixed

--- client/src/components/Poker/MoneyOnTable.jsx
+++ client/src/components/Poker/MoneyOnTable.jsx
@@ -5,7 +5,7 @@ const MoneyOnTable = ({userBet, dealerBet, moneyOnTable, dealerMove, buyIn}) => 
 
   return (
     <div>
-      <div>mney on table: {moneyOnTable}</div>
+      <div>Moey on table: {moneyOnTable}</div>
       <div>dealer chips  dealerMove: {dealerMove} this turn bet: {dealerBet}$ </div>
       <div>user chips this turn bet: {userBet}$   user money left: {buyIn} </div>
     </div>
