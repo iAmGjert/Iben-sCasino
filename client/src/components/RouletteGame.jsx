d524c8fe56e8d8519a2cdd212d6af411b2519b51
Eric Gjertsen <gjertsen.eric@gmail.com>
2022-06-01 12:34:27 -0500

    working test to see if user is logged in before displaying roulette components

--- client/src/components/RouletteGame.jsx
+++ client/src/components/RouletteGame.jsx
@@ -1,20 +1,32 @@
 import React, {useState, useEffect} from 'react';
 import RouletteWheel from './Roulette/RouletteWheel.jsx';
 import RouletteTable from './Roulette/RouletteTable.jsx';
+import axios from 'axios';
 
 
 const RouletteGame = () => {
   console.log('Roulette Game');
+  const [user, setUser] = useState(null);
+  useEffect(()=>{
+    axios.get('/routes/profile/user')
+      .then(( data )=>{
+        if (data.status === 201) {
+          setUser(data.data);
+          console.log(data.data);
+          return data.data;
+        }
+      }); 
+    
+  }, []);
   return (
     <div className='welcome'>
       <h1>Welcome to the roulette table!</h1>
       <span>
-        <RouletteWheel
-          user={ 'Me' }
-        />
-        <RouletteTable 
-          user={ 'Me' }
-        />
+        {
+          user ?
+            <div>User exists</div> :
+            <div>User does not exist</div>
+        }
       </span>
     </div>
   );
