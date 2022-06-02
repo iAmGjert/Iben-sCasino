09672b45b0586c21b8058930c2fc1cf60e4f2770
Braeden Ford <bford0002@gmail.com>
2022-06-02 13:55:38 -0500

    merge

--- client/src/components/Rewards.jsx
+++ client/src/components/Rewards.jsx
@@ -5,12 +5,13 @@ import axios from 'axios';
 
 const Rewards = ({ initUser }) => {
 
-  const [user, setUser] = useState(initUser)
+  const [user, setUser] = useState(null)
 
   const getProfile = () => {
     axios.get('/routes/profile/user')
       .then(user => {
         setUser(user.data)
+        console.log(user, 'getProfile');
       })
       .catch((err => console.log('getprof err', err)));
   }
@@ -22,34 +23,44 @@ const Rewards = ({ initUser }) => {
       }
     })
     .then(() => {
-      // console.log('put worked');
+      console.log('put worked');
     })
     .catch((err) => {
       console.error(err);
     })
   }
-
+  
+  useEffect(() => {
+    getProfile();
+  }, []);
+  
+  
   // useEffect(() => {
-  //   updateUser();
   //   getProfile();
-  // }, []);
+  // }, [user]);
 
+  // console.log(user);
 
 
   return (
     <h3>
+      
       <div>
         {
           user.money === 9600 ? <Link to='/rewards'><button className='rewardButton'>New Reward!</button></Link> : ''
         }
         </div>
+        
       <div>Welcome back!</div>
+
       <div>As a thank you, $200 has been added to your account!</div>
+
       <div>
         {
           user.money > 9600 ? <Link to='/rewards'><button className='rewardButton'>Check Rewards</button></Link> : ''
         }
       </div>
+
     </h3>
   )
 }
